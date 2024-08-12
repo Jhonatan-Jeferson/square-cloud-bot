37132e37722eda7b2ac85b161cb84d73837d81cd
Jhonatan-Jeferson <jhonatanjefersonoliveira@gmail.com>
2024-08-12 13:17:41 -0300

    Updating main file

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+from os import path, getenv
+from dotenv import load_dotenv
+from bot import config
 from bot.bot import SquareBot
-from bot.tokens import token_bot
+from uuid import UUID
 
-bot = SquareBot()
+dotenv_path: str = './bot/.env'
+language: dict[str, str] = config.RESPONSES.get(config.LANGUAGE)
 
-bot.run(token_bot)
+if not path.exists(dotenv_path):
+    print(language.get('dotenv_missing'))
+    bot_token = input(language.get('bot_token')).strip()
+    api_token = input(language.get('api_token')).strip()
+    with open(dotenv_path, 'w') as dotenv_file:
+        try:
+            UUID(api_token)
+        except ValueError as e:
+            print(language.get('invalid_token'))
+        dotenv_file.write(
+            f'BOT_TOKEN={bot_token}\n'+
+            f'API_TOKEN={api_token}'
+        ) 
+    del bot_token, api_token
+    
+load_dotenv()
+client = SquareBot(language)
+client.run(getenv('BOT_TOKEN'))
