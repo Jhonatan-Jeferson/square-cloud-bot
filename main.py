2e8ef7f1e6683cab9db58dd6d380aa54d14e9632
Jhonatan-Jeferson <jhonatanjefersonoliveira@gmail.com>
2024-08-20 12:12:23 -0300

    Only organizing the initialization through functions

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
-from os import getenv, path
-from uuid import UUID
+from os import getenv
 
 from dotenv import load_dotenv
 
 from bot import config
 from bot.bot import SquareBot
 
-dotenv_path: str = './bot/.env'
-language: dict[str, str] = config.RESPONSES.get(config.LANGUAGE)
 
-if not path.exists(dotenv_path):
-    print(language.get('dotenv_missing'))
-    bot_token = input(
-        language['initialization'].get('bot_token', str())
-    ).strip()
-    api_token = input(
-        language['initialization'].get('api_token', str())
-    ).strip()
-    with open(dotenv_path, 'w') as dotenv_file:
-        try:
-            UUID(api_token)
-        except ValueError:
-            print(language['initialization'].get('invalid_token'))
-        dotenv_file.write(
-            f'BOT_TOKEN={bot_token}\n' + f'API_TOKEN={api_token}'
-        )
-    del bot_token, api_token
+def check_for_environment(language: dict[str,str]) -> None:
+    """This function checks if you created the environment"""
+    from uuid import UUID
+    from os import path
+    
+    dotenv_path: str = './bot/.env'
+    if not path.exists(dotenv_path):
+        print(language.get('dotenv_missing'))
+        bot_token = input(
+            language['initialization'].get('bot_token', str())
+        ).strip()
+        api_token = input(
+            language['initialization'].get('api_token', str())
+        ).strip()
+        with open(dotenv_path, 'w') as dotenv_file:
+            try:
+                UUID(api_token)
+            except ValueError:
+                print(language['initialization'].get('invalid_token'))
+            dotenv_file.write(
+                f'BOT_TOKEN={bot_token}\nAPI_TOKEN={api_token}'
+            )
 
-load_dotenv()
-client = SquareBot(language)
-client.run(getenv('BOT_TOKEN'))
+def main() -> None:    
+    language: dict[str, str] = config.RESPONSES.get(config.LANGUAGE)
+    check_for_environment(language)
+    load_dotenv()
+    client = SquareBot(language)
+    client.run(getenv('BOT_TOKEN'))
+
+main()
